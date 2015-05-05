a742c304a038b1c6234a24a834a548277c19a8e6
KtodaZ <kylemail2003@gmail.com>
2015-05-04 21:57:02 -0700

    jQuery mousover works, doesn't do anything

--- pageScript.js
+++ pageScript.js
@@ -9,18 +9,32 @@ function getProfessorNames(className) {
     var cell      = document.getElementsByClassName(className); // Gives cell[i] an object containing data of className
     var professor = [];
 
-    for(var i =0; i < cell.length; i++) {
+    for(var i =0; i < 100; i++) {
         // Splits into separate string arrays seen here: http://imgur.com/fhgmsEm
         var profNameArray = [];
         profNameArray = cell[i].innerText.trim().split(/[ ]+/);
 
         if (profNameArray.length === 2) { //TODO: Additional exceptions for strings that contain non-name characters
-            console.log(profNameArray); // For debugging (use ctrl-shift-J)
             professor[i] = profNameArray[0] + " " + profNameArray[1];
+            //console.log(profNameArray); // For debugging (use ctrl-shift-J)
             console.log(professor[i]); // For debugging
-            //TODO: Jquery functionality goes here, using cell[i] object type
+
+            //TODO: Jquery functionality goes here, using cell[i] object type also see https://css-tricks.com/full-jquery-animations/
+            popup(cell[i]);
         }
     }
     return professor;
 }
+
+// jQuery animation popup
+function popup(cell) {
+    //TODO: This function works on mouse over, but it needs to be modified so that it will display a text box or whatever
+    $(cell).hover(function(){
+        $(this).filter(':not(:animated)').animate({ width: "200px" });
+    }, function() {
+        $(this).animate({ width: "100px" });
+        console.log("Test");
+    });
+}
+
 var professors = getProfessorNames('default1');
